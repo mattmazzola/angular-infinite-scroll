2ca3f6ecd335d6f5d592e1c79bcdfb2c140e074a
Matt Mazzola <mattm@microsoft.com>
2016-02-29 17:37:28 -0800

    Swap debounce for throttle with leading edge false.

--- src/directives/angular-infinite-scroll/angular-infinite-scroll.js
+++ src/directives/angular-infinite-scroll/angular-infinite-scroll.js
@@ -7,6 +7,7 @@ function AngularInifiteScrollController($scope, $timeout, $q) {
   this.infiniteScrollDelay = this.infiniteScrollDelay || 1000;
   this.containerElement = null;
   this.listElement = null;
+  this.scrollHandlerPromise = null;
     
   console.log('infiniteScrollOffset: ', this.infiniteScrollOffset);
   console.log('infiniteScrollDelay: ', this.infiniteScrollDelay);
@@ -22,13 +23,19 @@ function AngularInifiteScrollController($scope, $timeout, $q) {
     this.listElement = listElement;
     
     var scrollHandler = function () {
-        var shouldHandle = this.shouldInvokeScrollHandler(this.containerElement, this.listElement);
-        if(shouldHandle) {
-            this.infiniteScrollHandler();
+        var that = this;
+        var isListBottomAboveThreshold = this.isListBottomAboveThreshold(this.containerElement, this.listElement);
+        if(isListBottomAboveThreshold) {
+            this.scrollHandlerPromise = this.infiniteScrollHandler()
+                .then(function () {
+                    that.scrollHandlerPromise = null;
+                }, function () {
+                    that.scrollHandlerPromise = null;
+                })
         }
     };
     
-    this.containerElement.addEventListener("scroll", this.debounce(scrollHandler.bind(this), this.infiniteScrollDelay, true));
+    this.containerElement.addEventListener("scroll", this.throttle(scrollHandler.bind(this), this.infiniteScrollDelay, { leading: false, trailing: true }));
   }
 
   /**
@@ -42,35 +49,35 @@ function AngularInifiteScrollController($scope, $timeout, $q) {
   /**
    * Compute element boundaries and if bottom of list is above bottom of container + X offset then return true;
    */
-  this.shouldInvokeScrollHandler = function(containerElement, listElement) {
-      var shouldScroll = false;
+  this.isListBottomAboveThreshold = function(containerElement, listElement) {
+      var isListBottomAboveThreshold = false;
       
       if(!this.infiniteScrollDisabled) {
         console.group();
         // TODO: Maybe just require jQuery and calculate these correctly?
         var containerBottom = containerElement.getBoundingClientRect().top + containerElement.offsetHeight;
         var threshhold = containerBottom + this.infiniteScrollOffset;
-        console.log('threshhold: ' + containerBottom + ' = top (' + containerElement.getBoundingClientRect().top + ') + height (' + containerElement.offsetHeight + ') + offset (' + this.infiniteScrollOffset + ')');
+        console.log('threshhold: ' + threshhold + ' = top (' + containerElement.getBoundingClientRect().top + ') + height (' + containerElement.offsetHeight + ') + offset (' + this.infiniteScrollOffset + ')');
         var listBottom = listElement.getBoundingClientRect().top + listElement.offsetHeight;
         console.log('listBottom: ' + listBottom + ' = top (' + listElement.getBoundingClientRect().top + ') + height (' + listElement.offsetHeight + ')');
         
-        shouldScroll = (listBottom < threshhold);
-        console.log('ShouldInvokeScrollHandler: ', shouldScroll);
+        isListBottomAboveThreshold = (listBottom < threshhold);
+        console.log('isListBottomAboveThreshold: ', isListBottomAboveThreshold);
         console.groupEnd();
       }
       
-      return shouldScroll;
+      return isListBottomAboveThreshold;
   }
   
   /**
    * Check element dimensions, if necessary execute parent handler until container is filled
    */
-  this.checkInitialState = function () {
+  this.invokeHandlerUntilContainerIsFull = function () {
       var that = this;
-    function loadUntilShouldScroll() {
-        var shouldScroll = that.shouldInvokeScrollHandler(that.containerElement, that.listElement);
+    function invokeHandlerUntilContainerIsFullInner() {
+        var isListBottomAboveThreshold = that.isListBottomAboveThreshold(that.containerElement, that.listElement);
         var handlerPromise = new $q((resolve, reject) => reject());
-        if(shouldScroll) {
+        if(isListBottomAboveThreshold && !that.infiniteScrollDisabled) {
             handlerPromise = new $q(resolve => {
                 $timeout(function () {
                     return that.infiniteScrollHandler()
@@ -82,52 +89,48 @@ function AngularInifiteScrollController($scope, $timeout, $q) {
         }
         
         return handlerPromise
-            .then(() => loadUntilShouldScroll.call(that));
+            .then(() => invokeHandlerUntilContainerIsFullInner.call(that));
     }
     
-    if(this.infiniteScrollDisabled) {
+    if(!this.infiniteScrollDisabled) {
         $timeout(function () {
-            loadUntilShouldScroll();
+            invokeHandlerUntilContainerIsFullInner();
         });
     }
   }
     
-    /**
-     * Debounce from underscore, but swap setTimeout with $timeout
-     */
-    this.debounce = function(func, wait, immediate) {
-        var timeout, args, context, timestamp, result;
 
+    this.throttle = function(func, wait, options) {
+        var context, args, result;
+        var timeout = null;
+        var previous = 0;
+        if (!options) options = {};
         var later = function() {
-            var last = (new Date().getTime()) - timestamp;
-
-            if (last < wait && last >= 0) {
-                timeout = $timeout(later, wait - last);
-            } else {
-                timeout = null;
-                if (!immediate) {
-                    result = func.apply(context, args);
-                    if (!timeout) context = args = null;
-                }
-            }
+            previous = options.leading === false ? 0 : new Date().getTime();
+            timeout = null;
+            result = func.apply(context, args);
+            if (!timeout) context = args = null;
         };
-
         return function() {
+            var now = new Date().getTime();
+            if (!previous && options.leading === false) previous = now;
+            var remaining = wait - (now - previous);
             context = this;
             args = arguments;
-            timestamp = new Date().getTime();
-            var callNow = immediate && !timeout;
-            if (!timeout) {
-                timeout = $timeout(later, wait);
-            }
-            if (callNow) {
+            if (remaining <= 0 || remaining > wait) {
+                if (timeout) {
+                    $timeout.cancel(timeout);
+                    timeout = null;
+                }
+                previous = now;
                 result = func.apply(context, args);
-                context = args = null;
+                if (!timeout) context = args = null;
+            } else if (!timeout && options.trailing !== false) {
+                timeout = $timeout(later, remaining);
             }
-
             return result;
         };
-    };
+    }
 }
 
 function AngularInifiteScrollDirective() {
@@ -143,7 +146,7 @@ function AngularInifiteScrollDirective() {
     }
     
     controller.registerScrollHandler(scrollElement, element[0]);
-    controller.checkInitialState();
+    controller.invokeHandlerUntilContainerIsFull();
 
     $scope.$on('$destroy', function() {
       controller.unregisterScrollHandler();
